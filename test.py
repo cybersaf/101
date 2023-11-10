6e8fb044311517d1881f228e3276d6fe527b1e32
Safiyah Sheikh <safiyah@chainhaus.com>
2023-11-09 19:01:55 -0500

    update

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("test")
+memo = {}
+
+def fibonacci(n):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    else:       
+        result = fibonacci(n - 1) + fibonacci(n - 2)
+        memo[n] = result
+        return result
+    
+n = 10
+print(f"Fibonacci({n}) = {fibonacci(n)}")
+
